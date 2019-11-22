a9265030328c45bed4cbdfbcf0cb34ad15ba29d3
ChamMat <mathieu.chamoulaud@gmail.com>
2019-11-22 12:15:58 +0100

    ajout du prototype de la map

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>OLand</title>
+</head>
+<body>
+    <div class="panel">
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+        <div class="row">
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+            <div class="tile grass1"></div>
+            <div class="tile grass2"></div>
+        </div>
+    </div>
+</body>
+</html>
